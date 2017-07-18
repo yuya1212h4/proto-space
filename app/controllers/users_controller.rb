daafe8f6d61496d274b45f0dd50ba05946b3a66d
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-07-19 07:38:34 +0900

    fix indent

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   end
 
   def update
-      if current_user.update_without_current_password(user_params)
+    if current_user.update_without_current_password(user_params)
       redirect_to root_path, success: "Successfully edit your profile."
     else
       flash.now[:alert] = "Failure edit your profile."
