eef5d27c440c3ce92282f9964ffb55a149bc581d
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-07-19 02:02:57 +0900

    add_flash_types

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  add_flash_types :success, :info, :warning, :danger
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :member, :profile, :works, :user_image])
   end
