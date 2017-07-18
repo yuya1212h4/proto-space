5cc3b9d9ff4ff6ebcaf42a34e13631ad695307cf
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-07-18 17:25:20 +0900

    define update_without_current_password method on users.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+   def update_without_current_password(params, *options)
+     params.delete(:current_password)
+
+     if params[:password].blank? && params[:password_confirmation].blank?
+       params.delete(:password)
+       params.delete(:password_confirmation)
+     end
+
+     result = update_attributes(params, *options)
+     clean_up_passwords
+     result
+   end
+
 end
