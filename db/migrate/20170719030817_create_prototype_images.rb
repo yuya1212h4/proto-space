9a52be93680a27021845da9870edc1b2dce70b28
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-07-19 12:17:40 +0900

    rake db:migrate on ptototype_image.rb

--- db/migrate/20170719030817_create_prototype_images.rb
+++ db/migrate/20170719030817_create_prototype_images.rb
@@ -1,6 +1,9 @@
 class CreatePrototypeImages < ActiveRecord::Migration[5.0]
   def change
     create_table :prototype_images do |t|
+      t.string :image, null: false
+      t.integer :image_type, null: false
+      t.references :prototype, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170719023733) do
+ActiveRecord::Schema.define(version: 20170719030817) do
+
+  create_table "prototype_images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "image",        null: false
+    t.integer  "image_type",   null: false
+    t.integer  "prototype_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["prototype_id"], name: "index_prototype_images_on_prototype_id", using: :btree
+  end
 
   create_table "prototypes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "string",                  null: false
@@ -45,5 +54,6 @@ ActiveRecord::Schema.define(version: 20170719023733) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "prototype_images", "prototypes"
   add_foreign_key "prototypes", "users"
 end
