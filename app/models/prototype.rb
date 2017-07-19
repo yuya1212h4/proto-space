293986f99b3c7978feefc02585db3ae736d5e193
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-07-19 12:07:20 +0900

    create model prototype

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,2 +1,8 @@
 class Prototype < ApplicationRecord
+
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :prototype_images, dependent: :destroy
+
 end
