4eda5c66af1ed757bc8eaa2b790a461d9de7cf5c
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-07-20 22:32:00 +0900

    create prototype_controller.rb

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,8 +1,21 @@
 class PrototypesController < ApplicationController
   def index
+    @prototypes = Prototype.includes(:user).all
   end
 
   def new
+    @prototype = Prototype.new
+    @prototype.prototype_images.build
+  end
+
+  def create
+    @prototype = current_user.prototypes.new(prototype_params)
+    if @prototype.save
+      redirect_to prototype_path(@prototype)
+    else
+      flash[:alert] = 'prototype do not created' 
+      render :new
+    end
   end
 
   def show
@@ -10,4 +23,13 @@ class PrototypesController < ApplicationController
 
   def edit
   end
+
+  def update
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :text, prototype_images_attributes: [:image, :image_type])
+  end
+
 end
