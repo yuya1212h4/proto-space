5ad5ebbc2cf2dc8d3b49cc57e4cba8b5cab0ffc5
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-07-20 22:25:42 +0900

    add accepts_nested_attributes_for method

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -4,5 +4,8 @@ class Prototype < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :prototype_images, dependent: :destroy
+  accepts_nested_attributes_for :prototype_images
+
+  validates :title, presence: true
 
 end
