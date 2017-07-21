420dbae5db8daf8f37d518760f0288ef9ad37229
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-07-21 18:01:45 +0900

    add validates prototypeimage

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -4,8 +4,17 @@ class Prototype < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :prototype_images, dependent: :destroy
-  accepts_nested_attributes_for :prototype_images
+  accepts_nested_attributes_for :prototype_images, reject_if: :reject_posts
 
   validates :title, presence: true
+  validates :PrototypeImage, presence: true
+
+ def PrototypeImage
+   prototype_images[0].image if prototype_images[0].present?
+ end
+
+ def reject_posts(attributed)
+   attributed['image'].blank?
+ end
 
 end
