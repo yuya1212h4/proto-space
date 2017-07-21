b645a354f63da8e1dd6e67d9b32f4de0ddfafe1d
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-07-21 18:01:58 +0900

    add validates image

--- app/models/prototype_image.rb
+++ app/models/prototype_image.rb
@@ -1,8 +1,8 @@
 class PrototypeImage < ApplicationRecord
 
   belongs_to :prototype, optional: true
-
   enum image_type: { main: 0, sub: 1 }
-
   mount_uploader :image, ImageUploader
+
+  validates :image, presence: true
 end
