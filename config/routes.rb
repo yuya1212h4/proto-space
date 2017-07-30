d03ff7ecf19899182adad60c0a1939b7ccdacce3
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-07-30 19:19:42 +0900

    except prototypes/index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'prototypes#index'
-  resources :prototypes
+  resources :prototypes, except: :index
   resources :users, only: [:show, :edit, :update]
 end
