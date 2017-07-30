f69d1638f0590544c4f02c441dcf5f8f2a4d86ab
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-07-30 22:31:49 +0900

    add .page method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @prototypes = @user.prototypes
+    @prototypes = @user.prototypes.page(params[:page]).per(8)
   end
 
   def edit
