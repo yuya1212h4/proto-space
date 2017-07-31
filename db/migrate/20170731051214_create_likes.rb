200d7bef283b72e408d05873b8bf0a968a1543e1
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-07-31 14:18:57 +0900

    create likes table

--- db/migrate/20170731051214_create_likes.rb
+++ db/migrate/20170731051214_create_likes.rb
@@ -1,6 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[5.0]
   def change
     create_table :likes do |t|
+      t.references :user, foreign_key: true
+      t.references :prototype, foreign_key: true
 
       t.timestamps
     end
