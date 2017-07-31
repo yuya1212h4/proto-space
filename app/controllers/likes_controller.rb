866acc3520ac9588287dc1482b5f4098acec85fa
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-07-31 18:22:13 +0900

    add create and destroy on likes_contoller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,18 @@
 class LikesController < ApplicationController
+  before_action :set_prototype
+
+  def create
+    @like = @prototype.likes.create(user_id: current_user.id)
+  end
+
+  def destroy
+    like = @prototype.likes.find_by(user_id: current_user.id)
+    like.destroy
+  end
+
+  private
+  def set_prototype
+    @prototype = Prototype.find(params[:prototype_id])
+  end
+
 end
