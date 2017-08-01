a7cf6ec0dd23446577d8571202911d8c67d97453
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-08-01 11:39:14 +0900

    create comments create action on routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'prototypes#index'
   resources :prototypes, except: :index do
     resources :likes, only: [:create, :destroy]
+    resources :comments, only: :create
   end
   resources :users, only: [:show, :edit, :update]
 end
