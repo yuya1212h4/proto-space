7203c3d6d27938d0432681ef4d8fcab73312064f
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-08-01 11:47:35 +0900

    create create action on comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+
+  def create
+    @comment = current_user.comments.create(comment_params)
+    @comments = Prototype.find(params[:prototype_id]).comments.includes(:user)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content).merge(prototype_id: params[:prototype_id])
+  end
 end
