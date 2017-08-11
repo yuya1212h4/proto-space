4293f14238fd5511232153fae1115861bb26773d
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-08-11 14:03:19 +0900

    create spec/factories/prototype.rb

---
+++ spec/factories/prototype.rb
@@ -0,0 +1,16 @@
+FactoryGirl.define do
+  factory :prototype do
+    title              { Faker::Cat.name }
+    catch_copy         { Faker::Lorem.sentence }
+    text               { Faker::Lorem.sentence }
+    likes_count        0
+    created_at         { DateTime.now }
+    updated_at         { DateTime.now }
+    user
+
+    after(:create) do |prototype|
+      create(:prototype_image, :main_image, prototype: prototype)
+      create(:prototype_image, :sub_image, prototype: prototype)
+    end
+  end
+end
