dc13ada5b4d72b485eee5aa934278fdd4274a3c8
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-08-12 00:31:29 +0900

    add "has valid format jpg and jpeg and gif and png
    画像のアップロードについて、成功する時の拡張子のときについても追記

--- spec/models/prototype_image_spec.rb
+++ spec/models/prototype_image_spec.rb
@@ -6,6 +6,26 @@ describe PrototypeImage, type: :model do
   let(:prototype_image){ create(:prototype_image, :main_image, prototype: prototype) }
 
    describe "#create" do
+     it "has valid format jpg" do
+       prototype_image = build(:prototype_image, :main_image, image: fixture_file_upload("spec/fixtures/img/sample.jpg", 'image/jpg'))
+       expect(prototype_image).to be_valid
+     end
+
+     it "has valid format jpeg" do
+       prototype_image = build(:prototype_image, :main_image, image: fixture_file_upload("spec/fixtures/img/sample.jpeg", 'image/jpeg'))
+       expect(prototype_image).to be_valid
+     end
+
+     it "has valid format gif" do
+       prototype_image = build(:prototype_image, :main_image, image: fixture_file_upload("spec/fixtures/img/sample.gif", 'image/gif'))
+       expect(prototype_image).to be_valid
+     end
+
+     it "has valid format png" do
+       prototype_image = build(:prototype_image, :main_image, image: fixture_file_upload("spec/fixtures/img/sample.png", 'image/png'))
+       expect(prototype_image).to be_valid
+     end
+
      it "has the wrong content format" do
        prototype_image = build(:prototype_image, :main_image, image: fixture_file_upload("spec/fixtures/img/sample.tiff", 'image/tiff'))
        prototype_image.valid?
