6b549252e7ab0048d8cc67dd7c3be2c3b44b87e4
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-08-17 16:46:38 +0900

    GET #new on prototypes_controller_spec.rb

--- spec/controller/prototypes_controller_spec.rb
+++ spec/controller/prototypes_controller_spec.rb
@@ -25,4 +25,18 @@ describe PrototypesController, type: :controller do
         expect(response).to render_template :index
       end
     end
+
+    describe "GET #new" do
+      before do
+        get :new
+      end
+
+      it "assigns the requested prototype to @prototype" do
+        expect(assigns(:prototype)).to be_a_new(Prototype)
+      end
+
+      it "renders the :new template" do
+        expect(response).to render_template :new
+      end
+    end
 end
