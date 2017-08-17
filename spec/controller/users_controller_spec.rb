a32ec18d8bb9044b06bd4daf0591d8bda3592f59
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-08-17 16:53:04 +0900

    GET #show on users_controller_spec.rb

--- spec/controller/users_controller_spec.rb
+++ spec/controller/users_controller_spec.rb
@@ -3,3 +3,21 @@ require 'rails_helper'
 describe UsersController, type: :controller do
   let(:user){ create(:user) }
 
+  context "with user login" do
+    before do
+      sign_in user
+    end
+
+    describe "GET #show" do
+      before do
+        get :show, params: { id: user.id }
+      end
+
+      it "assigns the requested to @user" do
+        expect(assigns(:user)).to eq user
+      end
+
+      it "renders the :show templates" do
+        expect(response).to render_template :show
+      end
+    end
