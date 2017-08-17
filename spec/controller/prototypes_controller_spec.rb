d3dc07e8d60728c2a7bf0ccb379b046224e3c83d
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-08-17 16:46:17 +0900

    GET #index on prototypes_controller_spec.rb

--- spec/controller/prototypes_controller_spec.rb
+++ spec/controller/prototypes_controller_spec.rb
@@ -7,4 +7,22 @@ describe PrototypesController, type: :controller do
   let(:like){ create(:like, prototype: prototype) }
 
 
+  context "with usr login" do
+    before do
+      sign_in user
+    end
+
+    describe 'GET #index' do
+      before do
+        get :index
+      end
+
+      it 'assigns the requested prototypes to @prototypes' do
+        expect(assigns(:prototypes)).to include prototype
+      end
+
+      it 'renders the :index template' do
+        expect(response).to render_template :index
+      end
+    end
 end
