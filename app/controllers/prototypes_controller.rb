72b787a2d9ecf6fc90ea53f1ac5a96f44c951a93
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-08-18 16:44:27 +0900

    fix authenticate_user! on prototypes_controller.rb
    exceptの誤植の修正。

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, expect: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_prototype, only: [:show, :edit, :update, :destroy]
 
   def index
