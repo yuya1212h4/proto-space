986498218972d121434e42dc302b50f17a68ad02
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-10-12 11:20:02 +0900

    add process resize_to_fill on image_uploader.rb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -30,6 +30,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def scale(width, height)
   #   # do something
   # end
+  process resize_to_fill: [300, 300]
 
   # Create different versions of your uploaded files:
   version :thumb do
